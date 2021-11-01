4d1d9b5252465e8a3ad81ad37af819a8ccc63707
MaximeTB <92383989+MaximeTB@users.noreply.github.com>
2021-11-01 15:59:14 +0100

    Max

--- src/Heart.java
+++ src/Heart.java
@@ -6,6 +6,6 @@ public class Heart extends staticThing {
     private int x,y;
 
     public Heart(int x,int y){
-        super(x,y,"Heart3.png",0,0,30,25);
+        super(x,y,"heart5.jpg",0,0,225,225);
     }
 }
