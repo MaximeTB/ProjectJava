8cda67305041b3cc6c923a28d375529fd623f94a
MaximeTB <92383989+MaximeTB@users.noreply.github.com>
2021-11-03 11:51:38 +0100

    Sprite

--- src/Hero.java
+++ src/Hero.java
@@ -2,7 +2,7 @@ import javafx.scene.image.ImageView;
 
 public class Hero extends animatedThings{
     public Hero(){
-        super(200,150,"heros.png",20,10,60,85,1);
+        super(200,150,"heros.png",95,4,75,95,1,0,0,0,0,0);
     }
 
 }
--- src/animatedThings.java
+++ src/animatedThings.java
@@ -3,14 +3,22 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public abstract class animatedThings {
-    int px,py;
+    int px,py,att,ind,dur,indmax,sw,off;
     protected ImageView sprite;
 
-    public animatedThings(int x, int y, String fileName, int cropX, int cropY, int W, int L,int att){
-        px=x;
-        py=y;
+    public animatedThings(int x, int y, String fileName, int cropX, int cropY, int l, int L,int att,int ind,int dur,int indmax,int sw,int off){
+        this.px=x;
+        this.py=y;
+        this.att=att;
+        this.ind=ind;
+        this.dur=dur;
+        this.indmax=indmax;
+        this.sw=sw;
+        this.off=off;
+
+
         sprite=new ImageView(new Image(fileName));
-        sprite.setViewport(new Rectangle2D(cropX,cropY,W,L));
+        sprite.setViewport(new Rectangle2D(cropX,cropY,l,L));
 
     }
 
