ec9d5ba85032cb2893597223e3d3c36c81036e66
EleaMth <92917916+EleaMth@users.noreply.github.com>
2021-11-16 09:49:17 +0100

    version elea qui fonctionne (runner court et saute)

--- src/Foe.java
+++ src/Foe.java
@@ -1,6 +1,7 @@
 public class Foe extends animatedThings {
 
-    public Foe(int x, int y, String fileName, int cropX, int cropY, int l, int L, int att, int ind1, int ind2, int dur, int indmax, int sw, int off) {
-        super(x, y, fileName, cropX, cropY, l, L, att, ind1, ind2, dur, indmax, sw, off);
+    public Foe(int x, int y) {
+        super(x, y, "ennemi_détouré.png", 42, 17, 78, 58, 1, 1, 1, 0, 0, 0, 0);
+
     }
 }
